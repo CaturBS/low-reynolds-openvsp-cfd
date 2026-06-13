0f95313523f00262f3e2bd03d173c688c2344081
Catur Budi Santoso <ctrbudisantoso@gmail.com>
2026-06-13 23:44:15 +0700

    fix extractpolar

--- extractPolar.sh
+++ extractPolar.sh
@@ -2,19 +2,19 @@
 
 echo "alpha,CL,CD,CM" > polar.csv
 
-for D in polar/*
+for D in polar/alpha_*
 do
+    A=$(basename "$D" | sed 's/alpha_//')
 
-    A=$(basename $D | sed 's/alpha_//')
+    FILE=$(find "$D/postProcessing/forceCoeffs" -name coefficient.dat)
 
-    FILE=$(find $D/postProcessing/forceCoeffs -name coefficient.dat)
+    LAST=$(tail -1 "$FILE")
 
-    LAST=$(tail -1 $FILE)
-
-    CD=$(echo $LAST | awk '{print $2}')
-    CL=$(echo $LAST | awk '{print $4}')
-    CM=$(echo $LAST | awk '{print $10}')
+    CD=$(echo "$LAST" | awk '{print $2}')
+    CL=$(echo "$LAST" | awk '{print $5}')
+    CM=$(echo "$LAST" | awk '{print $8}')
 
     echo "$A,$CL,$CD,$CM" >> polar.csv
-
 done
+
+sort -n -t, -k1,1 polar.csv -o polar.csv
